6f609680e20f5660cbf59c6f626a7f49ae2e8c15
김민수 <alstn1181030@gmail.com>
2025-01-17 21:31:35 +0900

    [add] toggle 뼈대 추가

--- src/main/java/com/example/demo/toggle/FeatureToggleProvider.java
+++ src/main/java/com/example/demo/toggle/FeatureToggleProvider.java
@@ -5,4 +5,10 @@ import org.springframework.stereotype.Component;
 @Component
 public interface FeatureToggleProvider {
 
+    /**
+     * 해당 기능이 동작 가능한 상태인지 확인하는 메서드
+     * @param feature 메서드 이름
+     * @return 메서드 활성 여부
+     */
+    boolean isFeatureEnabled(String feature);
 }
