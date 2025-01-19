eea7786dfe6fbeb78d1285a6dfe4f9fdebca32ca
김민수 <alstn1181030@gmail.com>
2025-01-19 15:32:56 +0900

    [add] redis config 구현
    - 레디스 도커 컴포즈 추가
    - 레디스 설정 구현

---
+++ src/main/java/com/example/demo/config/RedisProperties.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisProperties {
+
+    @Value("${config.redis.port:6379}")
+    protected Integer port;
+
+    @Value("${config.redis.host:localhost}")
+    protected String host;
+
+}
