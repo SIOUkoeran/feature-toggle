44ecce0987dbb9a0d67852cd2128dfbbb48d6fa6
chammchi <alstn1181030@gmail.com>
2025-01-19 15:34:36 +0900

    [add] redis config 구현 (#3)
    - 레디스 도커 컴포즈 추가
    - 레디스 설정 구현

---
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+
+    // RedisConnectionFactory를 통해 Lettuce 연결을 설정
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        // 로컬 Redis 서버의 기본 설정 (호스트: localhost, 포트: 6379)
+        return new LettuceConnectionFactory(redisProperties.host, redisProperties.port);
+    }
+
+    // RedisTemplate 설정
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 키와 값을 String으로 직렬화
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+}
